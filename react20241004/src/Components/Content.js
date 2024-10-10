db2b7a25d0c1b16ce1eec545e4d20535e42ec69d
yhg1024 <dgr1024kr@naver.com>
2024-10-10 11:15:26 +0900

    추천수 수정

--- react20241004/src/Components/Content.js
+++ react20241004/src/Components/Content.js
@@ -1,33 +1,33 @@
 import { useState } from 'react';
 
+const FoodList = ({food}) => {
+
+    const [recomend, setRecomend] = useState(0);
+
+    const ThumbsUp = (count) => {
+        setRecomend((prev) => prev === 0 && count <0 ? 0 : prev + count)
+    }
+
+    return (
+        <li>
+            {food}
+            <sapn onClick={() => ThumbsUp(1)}>👍</sapn>
+            <span>추천 {recomend}</span>
+            <sapn onClick={() => ThumbsUp(-1)}>👎</sapn>
+        </li>
+    )
+}
+
 const Content = ({foodList}) => {
 
-    const [count, setCount] = useState(Object.values(foodList).flat().map(() => 0))
     return (
         <div>
-            { Object.keys(foodList).map((type, typeIndex) => 
+            { Object.keys(foodList).map((type) => 
                 <div className="blogContent">
                     <div className="contentTitle">{type}</div>
                     <ul className="content">
-                    {foodList[type].map((food, foodIndex) => 
-                            {
-                                const listIndex =  Object.values(foodList).flat().indexOf(food);
-                                return (
-                                <li key={foodIndex}>
-                                {food}
-                                <sapn onClick={() => {
-                                    const newCount = [...count]
-                                    newCount[listIndex]++
-                                    setCount(newCount)
-                                }}>👍</sapn>
-                                <span>추천 {count[listIndex]}</span>
-                                <sapn onClick={() => {
-                                    const newCount = [...count]
-                                    newCount[listIndex]--
-                                    setCount(newCount)
-                                }}>👎</sapn>
-                            </li>)
-                            }
+                    {foodList[type].map((food) => 
+                            <FoodList food={food}/>
                     )}
                     </ul>
                 </div>
